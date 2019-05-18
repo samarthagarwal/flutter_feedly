dd51b8152d4e15f0fe91d5c543fecffd86a44b7a
Samarth Agarwal <samarthagarwal@live.com>
2019-05-18 23:51:40 +0530

    Login Design Done

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -10,6 +10,171 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepOrange,
+      body: Form(
+        child: ListView(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 72.0, bottom: 36.0),
+              child: Icon(
+                Icons.rss_feed,
+                size: 180.0,
+                color: Colors.white,
+              ),
+            ),
+            Container(
+              margin:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              padding: const EdgeInsets.all(
+                4.0,
+              ),
+              decoration: BoxDecoration(
+                border: Border.all(
+                    color: Colors.white.withOpacity(0.5), width: 1.0),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10.0, horizontal: 15.0),
+                    child: Icon(
+                      Icons.alternate_email,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Container(
+                    height: 30.0,
+                    width: 1.0,
+                    color: Colors.white.withOpacity(0.5),
+                    margin: const EdgeInsets.only(right: 10.0),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Enter your email",
+                        hintStyle: TextStyle(
+                          color: Colors.white.withOpacity(0.5),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              padding: const EdgeInsets.all(
+                4.0,
+              ),
+              decoration: BoxDecoration(
+                border: Border.all(
+                    color: Colors.white.withOpacity(0.5), width: 1.0),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10.0, horizontal: 15.0),
+                    child: Icon(
+                      Icons.lock_open,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Container(
+                    height: 30.0,
+                    width: 1.0,
+                    color: Colors.white.withOpacity(0.5),
+                    margin: const EdgeInsets.only(right: 10.0),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Enter your password",
+                        hintStyle: TextStyle(
+                          color: Colors.white.withOpacity(0.5),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 20.0),
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: FlatButton(
+                      splashColor: Colors.white,
+                      color: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Expanded(
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 16.0),
+                              child: Text(
+                                "LOGIN",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: Colors.deepOrange,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 20.0),
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: FlatButton(
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Expanded(
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 16.0),
+                              child: Text(
+                                "Don't have an Account? Create one.",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
