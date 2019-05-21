0ee842ff95968c34edc9fbdba57c2640a4f6182b
Samarth Agarwal <samarthagarwal@live.com>
2019-05-21 23:17:25 +0530

    Signup design done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_feedly/pages/login.dart';
+import 'package:flutter_feedly/pages/signup.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
       ),
-      home: LoginPage(),
+      home: SignupPage(),
     );
   }
 }
--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -6,10 +6,271 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  TextEditingController _passwordConfirmController = TextEditingController();
+
+  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       backgroundColor: Colors.deepOrange,
+      body: Form(
+        child: ListView(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 72.0, bottom: 36.0),
+              child: Icon(
+                Icons.rss_feed,
+                size: 60.0,
+                color: Colors.white,
+              ),
+            ),
+            Container(
+              margin:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              padding: const EdgeInsets.all(
+                4.0,
+              ),
+              decoration: BoxDecoration(
+                border: Border.all(
+                    color: Colors.white.withOpacity(0.5), width: 1.0),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10.0, horizontal: 15.0),
+                    child: Icon(
+                      Icons.person,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Container(
+                    height: 30.0,
+                    width: 1.0,
+                    color: Colors.white.withOpacity(0.5),
+                    margin: const EdgeInsets.only(right: 10.0),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      controller: _nameController,
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Enter your name",
+                        hintStyle: TextStyle(
+                          color: Colors.white.withOpacity(0.5),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              padding: const EdgeInsets.all(
+                4.0,
+              ),
+              decoration: BoxDecoration(
+                border: Border.all(
+                    color: Colors.white.withOpacity(0.5), width: 1.0),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10.0, horizontal: 15.0),
+                    child: Icon(
+                      Icons.alternate_email,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Container(
+                    height: 30.0,
+                    width: 1.0,
+                    color: Colors.white.withOpacity(0.5),
+                    margin: const EdgeInsets.only(right: 10.0),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      controller: _emailController,
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Enter your email",
+                        hintStyle: TextStyle(
+                          color: Colors.white.withOpacity(0.5),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              padding: const EdgeInsets.all(
+                4.0,
+              ),
+              decoration: BoxDecoration(
+                border: Border.all(
+                    color: Colors.white.withOpacity(0.5), width: 1.0),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10.0, horizontal: 15.0),
+                    child: Icon(
+                      Icons.lock_open,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Container(
+                    height: 30.0,
+                    width: 1.0,
+                    color: Colors.white.withOpacity(0.5),
+                    margin: const EdgeInsets.only(right: 10.0),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      controller: _passwordController,
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Enter your password",
+                        hintStyle: TextStyle(
+                          color: Colors.white.withOpacity(0.5),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              padding: const EdgeInsets.all(
+                4.0,
+              ),
+              decoration: BoxDecoration(
+                border: Border.all(
+                    color: Colors.white.withOpacity(0.5), width: 1.0),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10.0, horizontal: 15.0),
+                    child: Icon(
+                      Icons.lock_open,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Container(
+                    height: 30.0,
+                    width: 1.0,
+                    color: Colors.white.withOpacity(0.5),
+                    margin: const EdgeInsets.only(right: 10.0),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      controller: _passwordController,
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Re-enter your password",
+                        hintStyle: TextStyle(
+                          color: Colors.white.withOpacity(0.5),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 20.0),
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: FlatButton(
+                      splashColor: Colors.white,
+                      color: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Expanded(
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 16.0),
+                              child: Text(
+                                "SIGN UP",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: Colors.deepOrange,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 20.0),
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: FlatButton(
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Expanded(
+                            child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 16.0),
+                              child: Text(
+                                "Already have an account? Login here.",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
