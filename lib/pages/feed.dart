2d29a4551d1d179fccd5cb5c2d3e412213bdeaab
Samarth Agarwal <samarthagarwal@live.com>
2019-07-27 16:01:01 +0530

    displaying feed done

--- lib/pages/feed.dart
+++ lib/pages/feed.dart
@@ -17,13 +17,19 @@ class _FeedPageState extends State<FeedPage> {
   Firestore _firestore = Firestore.instance;
   FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
-  _navigateToCreatePage() {
-    Navigator.push(context, MaterialPageRoute(builder: (BuildContext ctx) {
+  _navigateToCreatePage() async {
+    await Navigator.push(context,
+        MaterialPageRoute(builder: (BuildContext ctx) {
       return CreatePage();
     }));
+
+    // Refresh the feed
+    _getFeedFuture = _getFeed();
   }
 
   Future _getFeed() async {
+    _posts = [];
+
     Query _query = _firestore
         .collection("posts")
         .orderBy("created", descending: true)
@@ -34,6 +40,15 @@ class _FeedPageState extends State<FeedPage> {
     print(_querySnapshot.documents.length);
 
     _postDocuments = _querySnapshot.documents;
+
+    for (int i = 0; i < _postDocuments.length; ++i) {
+      Widget w = ListTile(
+        title: Text(_postDocuments[i].data["text"]),
+        subtitle: Text(_postDocuments[i].data["owner_name"]),
+      );
+
+      _posts.add(w);
+    }
 
     return _postDocuments;
   }
@@ -48,6 +63,47 @@ class _FeedPageState extends State<FeedPage> {
 
     _items.add(_composeBox);
 
+    Widget separator = Container(
+      padding: const EdgeInsets.all(10),
+      child: Text(
+        "Recent Posts",
+        style: TextStyle(
+          color: Colors.black54,
+        ),
+      ),
+    );
+
+    _items.add(separator);
+
+    Widget feed = FutureBuilder(
+        future: _getFeedFuture,
+        builder: (BuildContext ctx, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting ||
+              snapshot.data == null) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                CircularProgressIndicator(),
+                SizedBox(
+                  height: 16.0,
+                ),
+                Text("Loading..."),
+              ],
+            );
+          } else if (snapshot.data.length == 0) {
+            return Text("No data to display");
+          } else {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: _posts,
+            );
+          }
+        });
+
+    _items.add(feed);
+
     return _items;
   }
 
@@ -55,7 +111,7 @@ class _FeedPageState extends State<FeedPage> {
   void initState() {
     super.initState();
 
-    _getFeed();
+    _getFeedFuture = _getFeed();
   }
 
   @override
